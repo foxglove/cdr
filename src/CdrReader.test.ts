2bfbcf2d38c73c1e2a08093b473454d550e3f499
John Hurliman <jhurliman@jhurliman.org>
2021-06-28 17:32:22 -0700

    Read typed arrays

--- src/CdrReader.test.ts
+++ src/CdrReader.test.ts
@@ -1,4 +1,16 @@
 import { CdrReader } from "./CdrReader";
+import { CdrWriter } from "./CdrWriter";
+
+type ArrayGetter =
+  | "int8Array"
+  | "uint8Array"
+  | "int16Array"
+  | "uint16Array"
+  | "int32Array"
+  | "uint32Array"
+  | "float32Array"
+  | "float64Array";
+type Setter = "int8" | "uint8" | "int16" | "uint16" | "int32" | "uint32" | "float32" | "float64";
 
 // Example tf2_msgs/TFMessage
 const tf2_msg__TFMessage =
@@ -44,4 +56,76 @@ describe("CdrReader", () => {
     expect(reader.float64()).toBeCloseTo(3.835);
     expect(reader.float64()).toBeCloseTo(0);
   });
+
+  it.each([
+    ["int8Array", "int8", [-128, 127, 3]],
+    ["uint8Array", "uint8", [0, 255, 3]],
+    ["int16Array", "int16", [-32768, 32767, -3]],
+    ["uint16Array", "uint16", [0, 65535, 3]],
+    ["int32Array", "int32", [-2147483648, 2147483647, 3]],
+    ["uint32Array", "uint32", [0, 4294967295, 3]],
+  ])("reads %s", (getter: string, setter: string, expected: number[]) => {
+    const writer = new CdrWriter();
+    writeArray(writer, setter as Setter, expected);
+
+    const reader = new CdrReader(writer.data);
+    const array = reader[getter as ArrayGetter](reader.sequenceLength());
+    expect(Array.from(array.values())).toEqual(expected);
+  });
+
+  it.each([
+    ["float32Array", "float32", [-3.835, 0, Math.PI], 6],
+    ["float64Array", "float64", [-3.835, 0, Math.PI], 15],
+    ["float64Array", "float64", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -0.123456789121212121212], 15],
+  ])("reads %s", (getter: string, setter: string, expected: number[], numDigits: number) => {
+    const writer = new CdrWriter();
+    writeArray(writer, setter as Setter, expected);
+
+    const reader = new CdrReader(writer.data);
+    const array = reader[getter as ArrayGetter](reader.sequenceLength());
+    expectToBeCloseToArray(Array.from(array.values()), expected, numDigits);
+  });
+
+  it.each([
+    ["int64Array", "int64", [-9223372036854775808n, 9223372036854775807n, 3n]],
+    ["uint64Array", "uint64", [0n, 18446744073709551615n, 3n]],
+    ["uint64Array", "uint64", [1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n, 10n, 11n, 12n]],
+  ])("reads %s", (getter: string, setter: string, expected: bigint[]) => {
+    const writer = new CdrWriter();
+    writeBigArray(writer, setter as "int64" | "uint64", expected);
+
+    const reader = new CdrReader(writer.data);
+    const array = reader[getter as "int64Array" | "uint64Array"](reader.sequenceLength());
+    expect(Array.from(array.values())).toEqual(expected);
+  });
+
+  it("reads stringArray", () => {
+    const writer = new CdrWriter();
+    writer.sequenceLength(3);
+    writer.string("abc");
+    writer.string("");
+    writer.string("test string");
+
+    const reader = new CdrReader(writer.data);
+    expect(reader.stringArray(reader.sequenceLength())).toEqual(["abc", "", "test string"]);
+  });
 });
+
+function writeArray(writer: CdrWriter, setter: Setter, array: number[]): void {
+  writer.sequenceLength(array.length);
+  for (const value of array) {
+    writer[setter](value);
+  }
+}
+
+function writeBigArray(writer: CdrWriter, setter: "int64" | "uint64", array: bigint[]): void {
+  writer.sequenceLength(array.length);
+  for (const value of array) {
+    writer[setter](value);
+  }
+}
+
+function expectToBeCloseToArray(actual: number[], expected: number[], numDigits: number): void {
+  expect(actual.length).toBe(expected.length);
+  actual.forEach((x, i) => expect(x).toBeCloseTo(expected[i]!, numDigits));
+}
