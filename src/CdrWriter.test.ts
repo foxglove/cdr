d054143785faf075c179448bd7488a6f457ba4c6
John Hurliman <jhurliman@jhurliman.org>
2023-05-03 21:26:07 -0700

    lint

--- src/CdrWriter.test.ts
+++ src/CdrWriter.test.ts
@@ -169,6 +169,6 @@ describe("CdrWriter", () => {
   });
 });
 
-function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+function enumKeys<O extends Record<string, unknown>, K extends keyof O = keyof O>(obj: O): K[] {
   return Object.keys(obj).filter((k) => Number.isNaN(+k)) as K[];
 }
