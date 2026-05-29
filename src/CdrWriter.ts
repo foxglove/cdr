40b81c27ccc4408fc853ba80f42de40ddb42734f
Miles Egan <miles+github@foxglove.dev>
2026-05-29 15:43:32 -0600

    Restore UTF-8 length comments (#37)

--- src/CdrWriter.ts
+++ src/CdrWriter.ts
@@ -21,21 +21,25 @@ function stringLengthUtf8(str: string): number {
   let byteLength = 0;
   const numCodeUnits = str.length;
   for (let i = 0; i < numCodeUnits; i++) {
-    const codeUnit = str.charCodeAt(i);
+    const codeUnit = str.charCodeAt(i); // 0x0000-0xFFFF
     if (codeUnit <= 0x7f) {
-      byteLength += 1;
+      byteLength += 1; // 0b0xxxxxxx
     } else if (codeUnit <= 0x7ff) {
-      byteLength += 2;
+      byteLength += 2; // 0b110xxxxx 0b10xxxxxx
     } else if (0xd800 <= codeUnit && codeUnit <= 0xdbff) {
+      // If the input string is valid UTF-16 then these surrogate characters come in pairs. They
+      // represent code points in the range 0x100000-0x10ffff and are represented with 4 bytes in
+      // UTF-8.
       const nextCodeUnit = str.charCodeAt(i + 1);
       if (0xdc00 <= nextCodeUnit && nextCodeUnit <= 0xdfff) {
-        byteLength += 4;
+        byteLength += 4; // 0b11110xxx 0b10xxxxxx 0b10xxxxxx 0b10xxxxxx
         i++;
       } else {
-        byteLength += 3;
+        byteLength += 3; // 0b1110xxxx 0b10xxxxxx 0b10xxxxxx
       }
     } else {
-      byteLength += 3;
+      // <= 0xFFFF
+      byteLength += 3; // 0b1110xxxx 0b10xxxxxx 0b10xxxxxx
     }
   }
   return byteLength;
